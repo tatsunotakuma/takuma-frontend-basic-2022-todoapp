538e6459996817b4dcaf3bf3127552b84b1e0aa9
tatsunotakuma <tatsuno.takuma@gmail.com>
2024-03-03 18:25:54 +0900

    Atoms/AddTaskButtonの作成

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import FONTFAMILY from "../../../variables/font_family";
+import TEXT from "../../../variables/texts";
+import plus from "../../../assets/svg/plus.svg";
+
+export const AddTaskButton = ({ onClick }) => {
+  return (
+    <StyledButton onClick={onClick}>
+      <StyledImage src={plus} alt="add icon" />
+      <StyledAddTask>タスクを追加</StyledAddTask>
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+   {
+    display: flex;
+    padding: 2px 6px;
+    gap: 10px;
+    cursor: pointer;
+    border: none;
+    border-radius: 12px;
+    background-color: ${COLOR.LIGHT_GRAY_TRANSPARENT};
+    transition: background-color 0.3 ease;
+
+    :hover {
+      background-color: ${COLOR.GREEN_TRANSLUCENT};
+    }
+  }
+`;
+
+const StyledImage = styled.img`
+   {
+    width: 20px;
+  }
+`;
+
+const StyledAddTask = styled.div`
+   {
+    color: ${COLOR.GREEN};
+    font-family: ${FONTFAMILY.NOTO_SANS};
+    ${TEXT.S};
+  }
+`;
